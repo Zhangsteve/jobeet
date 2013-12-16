54d163813f7d76147db391a87fe64b4b85ac7ccf
Zhangsteve <seakiro@msn.com>
2013-12-16 10:26:48 +0100

    test gitignore

--- src/Ens/JobeetBundle/Entity/Enquiry.php
+++ src/Ens/JobeetBundle/Entity/Enquiry.php
@@ -2,6 +2,11 @@
 // src/Ens/JobeetBundle/Entity/Enquiry.php
 
 namespace Ens\JobeetBundle\Entity;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\MinLength;
+use Symfony\Component\Validator\Constraints\MaxLength;
 
 class Enquiry
 {
@@ -51,5 +56,17 @@ class Enquiry
     public function setBody($body)
     {
         $this->body = $body;
+    }
+// confirmer les champs de form contact.
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+      $metadata->addPropertyConstraint('name', new NotBlank());
+
+      $metadata->addPropertyConstraint('email', new Email());
+
+      $metadata->addPropertyConstraint('subject', new NotBlank());
+      $metadata->addPropertyConstraint('subject', new MaxLength(50));
+
+      $metadata->addPropertyConstraint('body', new MinLength(50));
     }
 }
